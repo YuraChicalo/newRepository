5a18470faeecc4b18e8f95a6270f7becadb57d50
yura chicalo <yura.chicalo@ukr.net>
2023-03-14 18:04:30 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TrialComponent from "./Components/TrialComponent/TrialComponent";
+import Component2 from "./Components/Component2/Component2";
+
+function App() {
+    return (
+        <div className="App">
+            <TrialComponent/>
+            <Component2/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Components/TrialComponent/TrialComponent.jsx
@@ -0,0 +1,11 @@
+import s from './TrialComponent.module.css'
+
+function TrialComponent() {
+    return (
+        <div className="App">
+            <h1 className={s.color}>Yura Chicalo</h1>
+        </div>
+    );
+}
+
+export default TrialComponent;
