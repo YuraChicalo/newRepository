69c85422bd0eb2c0ff4591839ed146c209f9f5d5
yura chicalo <yura.chicalo@ukr.net>
2023-03-14 18:17:37 +0200

    init commit

---
+++ src/Components/Component3/Component2.jsx
@@ -0,0 +1,11 @@
+import s from './Component2.css'
+
+const Component2 = () => {
+    return (
+        <div className="App">
+            <h1 className={s.color}>Component2</h1>
+        </div>
+    );
+}
+
+export default Component2;
